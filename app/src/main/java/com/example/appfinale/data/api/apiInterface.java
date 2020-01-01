d5ab98b5566f9a64c0210377faf726387d70506f
bakkari1u <mohamedbakkari2013@gmail.com>
2020-01-01 22:46:02 +0100

    add model for api news

---
+++ app/src/main/java/com/example/appfinale/data/api/apiInterface.java
@@ -0,0 +1,4 @@
+package com.example.appfinale.data.api;
+
+public class apiInterface {
+}
---
+++ app/src/main/java/com/example/appfinale/data/di/ApiClient.java
@@ -0,0 +1,44 @@
+package com.example.appfinale.data.di;
+
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+import com.google.gson.Gson;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+
+    public static final String BASE_URL = "https://newsapi.org/v2/";
+    public static Retrofit retrofit;
+    private static Gson gson;
+
+    public static Retrofit getRetrofit() {
+        if (retrofit == null) {
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(interceptor)
+                    .addNetworkInterceptor(new StethoInterceptor())
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create(getGson()))
+                    .build();
+        }
+        return retrofit;
+    }
+
+    public static Gson getGson() {
+        if (gson == null) {
+            gson = new Gson();
+        }
+        return gson;
+    }
+
+}
