0998a3af7b50bd102b3a53e1c0a81f76910cab0f
bakkari1u <mohamedbakkari2013@gmail.com>
2020-01-01 23:12:57 +0100

    add apiinterface

--- app/src/main/java/com/example/appfinale/data/api/apiInterface.java
+++ app/src/main/java/com/example/appfinale/data/api/apiInterface.java
@@ -1,4 +1,18 @@
 package com.example.appfinale.data.api;
 
-public class apiInterface {
+import com.example.appfinale.data.api.model.NewsResponse;
+
+import io.reactivex.Single;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface apiInterface {
+
+    @GET("top-headlines")
+    Single<NewsResponse> getNews(
+
+            @Query("country") String country ,
+            @Query("apiKey") String apiKey
+
+    );
 }
